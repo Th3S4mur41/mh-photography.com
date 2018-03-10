0c47420970645bcb1ecf26828e23b200a0d5b743
Michael Vanderheyden <michael.vanderheyden@unify.com>
2018-03-10 19:56:12 +0100

    add WebStorm config to gitignore

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -5,11 +5,15 @@
 (function () {
 	'use strict';
 
-	document.addEventListener('scroll', function () {
+	const headerSchrink = function () {
 		if (window.scrollY > 200) {
 			document.getElementById('header').classList.add('shrink');
 		} else {
 			document.getElementById('header').classList.remove('shrink');
 		}
+	};
+
+	document.addEventListener('scroll', function () {
+		headerSchrink();
 	});
 }());
