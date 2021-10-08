27c032b48b9829e2cb9972566e8a5b728c31acae
Michaël Vanderheyden <michael.vanderheyden@atos.net>
2021-10-08 15:52:51 +0200

    refactor: compile & run with vite

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -3,6 +3,8 @@
  * Copyright © 2020 Mouch.net
  */
 
+import '../styles/style.css';
+
 (function () {
 	'use strict';
 	const categories = ['category-1', 'category-2', 'category-3', 'category-4', 'category-5', 'category-6'];
@@ -15,7 +17,7 @@
 		try {
 			categories.forEach(function (category) {
 				const cat = document.getElementById(category);
-				const link = cat.children[0];
+				const link = cat?.children[0];
 
 				if (link) {
 					link.addEventListener('click', onCategoryClicked);
@@ -54,13 +56,13 @@
 	 * onCategoryClicked
 	 * @returns {void}
 	 */
-	function onCategoryClicked() {
+	function onCategoryClicked(this: HTMLElement) {
 		try {
-			const carousel = this.nextSibling;
+			const carousel = this.nextElementSibling as HTMLElement;
 
-			carousel.addEventListener('click', onCarouselClicked);
-			carousel.classList.remove('hidden');
-			carousel.focus();
+			carousel?.addEventListener('click', onCarouselClicked);
+			carousel?.classList.remove('hidden');
+			carousel?.focus();
 		} catch (e) {}
 	}
 
@@ -68,10 +70,10 @@
 	 * onCarouselClicked
 	 * @returns {void}
 	 */
-	function onCarouselClicked() {
+	function onCarouselClicked(this: HTMLElement) {
 		try {
 			this.classList.add('hidden');
-			this.removeListener('click');
+			this.removeEventListener('click', onCarouselClicked);
 		} catch (e) {}
 	}
 
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+
+module.exports = defineConfig({
+	root: 'src',
+	publicDir: '../public',
+	build: {
+		outDir: '../dist'
+	},
+	css: {},
+	plugins: [
+		handlebars({
+			context: {
+				BUILD: '<%= buildnumber %>',
+				YEAR: new Date().getFullYear()
+			}
+		})
+	]
+});
