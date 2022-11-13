ab286b1dd823163c93a3394594be4ccbd83f23bd
Michaël Vanderheyden <michael.vanderheyden@atos.net>
2022-11-13 13:19:05 +0100

    refactor: add initial album view

---
+++ tools/photos.js
@@ -0,0 +1,71 @@
+/**
+ * tools/photos.js
+ */
+
+const path = require('path');
+const fs = require('fs');
+const sizeOf = require('image-size');
+const projectRoot = '../';
+
+/**
+ * getPhotos
+ * @param {string} input The path of the photo albums
+ * @returns {JSON} the JSON representations of the photo albums
+ */
+export const photos = (input) => {
+	input = input || 'assets/img/albums';
+	const albumsPath = path.join(__dirname, projectRoot, input);
+
+	const photos = {};
+
+	// Get a list of albums
+	const albums = fs
+		.readdirSync(albumsPath, { withFileTypes: true })
+		.filter((item) => {
+			return item.isDirectory();
+		})
+		.map((item) => {
+			return item.name;
+		});
+
+	// Get a list of file in each album
+	albums.forEach((album) => {
+		photos[album] = {};
+		photos[album].path = `${input}/${album}`;
+		photos[album].images = [];
+
+		const images = fs
+			.readdirSync(path.join(__dirname, projectRoot, photos[album].path), {
+				withFileTypes: true
+			})
+			.filter((item) => {
+				return item.isFile();
+			})
+			.map((item) => {
+				return item.name;
+			});
+
+		images.forEach((image) => {
+			const img = {};
+
+			img.name = image;
+			img.path = `/${photos[album].path}/${image}`;
+
+			const dimensions = sizeOf(path.join(__dirname, projectRoot, img.path));
+			img.width = dimensions.width;
+			img.height = dimensions.height;
+			img.orientation = dimensions.width > dimensions.height ? 'landscape' : 'portrait';
+
+			photos[album].images.push(img);
+		});
+	});
+
+	return photos;
+};
+
+export const writePhotosFile = (input, output) => {
+	fs.writeFile(output || 'photos.json', JSON.stringify(photos(input)), () => {
+		// Checking for errors
+		console.log('Done writing'); // Success
+	});
+};
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,70 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
+import { photos } from './tools/photos.js';
 import { resolve } from 'path';
 
+const root = resolve(__dirname, 'src');
+const albums = photos('assets/img/albums');
+
+const pageData = {
+	'/index.html': {
+		navPath: ''
+	},
+	'/albums/index.html': {
+		navPath: '/'
+	},
+	'/albums/business/index.html': {
+		navPath: '/',
+		title: 'Business',
+		photos: albums['business'].images
+	},
+	'/albums/families-and-kids/index.html': {
+		navPath: '/',
+		title: 'Families & Kids',
+		photos: albums['families-and-kids'].images
+	},
+	'/albums/people-and-lifestyle/index.html': {
+		navPath: '/',
+		title: 'People & Lifestyle',
+		photos: albums['people-and-lifestyle'].images
+	},
+	'/albums/travel/index.html': {
+		navPath: '/',
+		title: 'Travel',
+		photos: albums['travel'].images
+	},
+	'/albums/weddings/index.html': {
+		navPath: '/',
+		title: 'Weddings',
+		photos: albums['weddings'].images
+	}
+};
+
 module.exports = defineConfig({
-	root: 'src',
+	root: root,
 	publicDir: '../public',
 	build: {
-		outDir: '../dist'
+		outDir: '../dist',
+		rollupOptions: {
+			input: {
+				main: resolve(root, 'index.html'),
+				albums: resolve(root, 'albums', 'index.html'),
+				business: resolve(root, 'albums/business', 'index.html')
+			}
+		}
 	},
 	preview: {
 		port: 8080
 	},
 	plugins: [
 		handlebars({
-			context: {
-				currentYear: new Date().getFullYear(),
-				version: `v${process.env.npm_package_version}`
+			context(pagePath) {
+				return {
+					currentYear: new Date().getFullYear(),
+					version: `v${process.env.npm_package_version}`,
+					context: pageData[pagePath]
+				};
 			},
 			partialDirectory: resolve(__dirname, 'src/partials')
 		})
