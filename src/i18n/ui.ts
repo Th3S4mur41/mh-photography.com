a3add4d75d85dd147523cfc233921896626ef1dd
Michaël Vanderheyden <hello@th3s4mur41.me>
2023-07-29 21:47:56 +0200

    feat: add localization for header  and navigation

---
+++ src/i18n/ui.ts
@@ -0,0 +1,46 @@
+export const languages = {
+	de: 'Deutsch',
+	en: 'English',
+	es: 'Español',
+	fr: 'Français'
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+	de: {
+		'nav.about': 'Über mich',
+		'nav.photos': 'Gallerien',
+		'nav.weddings': 'Hochzeiten',
+		'nav.contact': 'Kontakt',
+		'header.title': `Hallo! Ich bin Manuel, Fotograf`,
+		'header.subtitle': 'Hochzeitsfotografie - Events - Familien - Portraits - Editorials'
+	},
+	en: {
+		'nav.home': 'Home',
+		'nav.about': 'About me',
+		'nav.photos': 'Gallery',
+		'nav.weddings': 'Weddings',
+		'nav.videos': 'Videos',
+		'nav.contact': 'Contact',
+		'header.title': `Hi! I'm Manuel, photographer in Barcelona`,
+		'header.subtitle': 'Wedding photography - event - families - portraits - publications'
+	},
+	es: {
+		'nav.home': 'Inicio',
+		'nav.about': 'Sobre mí',
+		'nav.photos': 'Galerías',
+		'nav.weddings': 'Bodas',
+		'nav.contact': 'Contacto',
+		'header.title': `Hola! Soy Manuel, fotógrafo en Barcelona`,
+		'header.subtitle': 'Fotografía de bodas - eventos - familias - retratos - editoriales'
+	},
+	fr: {
+		'nav.home': 'Accueil',
+		'nav.about': 'À propos',
+		'nav.photos': 'Galeries',
+		'nav.weddings': 'Mariages',
+		'header.title': `Bonjour! Je suis Manuel, photographe`,
+		'header.subtitle': 'Photographie de mariage - événements - familles - portraits - éditoriaux'
+	}
+} as const;
