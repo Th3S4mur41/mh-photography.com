79f5ef71be920fc7d7e927f2499df6dac172e0b6
Michaël Vanderheyden <hello@th3s4mur41.me>
2023-08-11 16:30:33 +0200

    feat: hide default language in url

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { defaultLang, ui } from './ui';
+import { defaultLang, showDefaultLang, ui } from './ui';
 
 export function getLangFromUrl(url: URL) {
 	const [, lang] = url.pathname.split('/');
@@ -8,8 +8,20 @@ export function getLangFromUrl(url: URL) {
 	return defaultLang;
 }
 
+export function getLangPath() {
+	return function path(lang: string) {
+		return !showDefaultLang && lang === defaultLang ? '/' : `/${lang}`;
+	};
+}
+
 export function useTranslations(lang: keyof typeof ui) {
 	return function t(key: keyof (typeof ui)[typeof defaultLang]) {
 		return ui[lang][key] || ui[defaultLang][key];
+	};
+}
+
+export function useTranslatedPath(lang: keyof typeof ui) {
+	return function translatePath(path: string, l: string = lang) {
+		return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`;
 	};
 }
