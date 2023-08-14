f4f2c73212e134c203b07adde7ffef496b0c2367
Michaël Vanderheyden <hello@th3s4mur41.me>
2023-08-14 10:46:13 +0200

    feat: add initial layout for new about section (#678)
    * feat: add initial layout for new about section
    
    * feat: impove image layout
    
    * chore(about): change title to avoid repetition
    
    * style: fix liniting issues

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,8 +1,8 @@
 export const languages = {
-	de: 'Deutsch',
-	en: 'English',
-	es: 'Español',
-	fr: 'Français'
+	de: `Deutsch`,
+	en: `English`,
+	es: `Español`,
+	fr: `Français`
 };
 
 export const defaultLang = 'en';
@@ -41,10 +41,10 @@ export const routes = {
 
 export const ui = {
 	de: {
-		'nav.about': 'Über mich',
-		'nav.photos': 'Fotos',
-		'nav.weddings': 'Hochzeiten',
-		'nav.contact': 'Kontakt',
+		'nav.about': `Über mich`,
+		'nav.photos': `Fotos`,
+		'nav.weddings': `Hochzeiten`,
+		'nav.contact': `Kontakt`,
 		'header.title': `Hallo! Ich bin Manuel, Fotograf`,
 		'header.subtitle': 'Hochzeitsfotografie - Events - Familien - Portraits - Editorials',
 		'photos.title': 'Fotos',
@@ -55,17 +55,35 @@ export const ui = {
 		'photos.weddings': 'Hochzeiten'
 	},
 	en: {
-		'meta.keywords':
-			'photography, photographer, Manuel Hamel, weddings, people, lifestyle, travel, families, kids, business, studio, photo, portrait, art, artistic, pictures, photoshop, Barcelona',
-		'meta.description': 'Belgian photographer, based in Barcelona, who loves to capture moments and to travel!',
-		'nav.home': 'Home',
-		'nav.about': 'About me',
-		'nav.photos': 'Photos',
-		'nav.weddings': 'Weddings',
-		'nav.videos': 'Videos',
-		'nav.contact': 'Contact',
+		'meta.keywords': `photography, photographer, Manuel Hamel, weddings, people, lifestyle, travel, families, kids, business, studio, photo, portrait, art, artistic, pictures, photoshop, Barcelona`,
+		'meta.description': `Belgian photographer, based in Barcelona, who loves to capture moments and to travel!`,
+		'nav.home': `Home`,
+		'nav.about': `About me`,
+		'nav.photos': `Photos`,
+		'nav.weddings': `Weddings`,
+		'nav.videos': `Videos`,
+		'nav.contact': `Contact`,
 		'header.title': `Hi! I'm Manuel, photographer in Barcelona`,
-		'header.subtitle': 'Wedding photography - event - families - portraits - publications',
+		'header.subtitle': `Wedding photography - event - families - portraits - publications`,
+		'about.title': `A little something about me`,
+		'about.p1': `A passionate wedding, event, family, portrait, and editorial photographer and videographer.`,
+		'about.p2': `Each photograph is much more to me than just pressing a button and capturing an image. It is an art that allows me to capture emotions, immortalize stories, and create authentic connections.`,
+		'about.p3': `I want you to feel comfortable in front of the camera and enjoy every moment we share. Because not only will I be your photographer, but when we work together, I will also be the friend who brings out your best side. For that, we will create a safe and trusting space in which you not only have fun and be yourself in front of the camera, but I will also guide you to a relaxed and pressure-free atmosphere.`,
+		'about.p4': `You don't need to be a professional model to look amazing in your photos; I will be behind the lens to highlight the best in you. I believe that trust and empathy are key to achieving natural and authentic photographs. That's why each photo session is unique, just like you and the people who accompany you. I love getting to know each client, listening to their stories, and capturing the essence of who they are.`,
+		'about.p5': `I will not only be attentive to the most important shots but also to those small 
+details and gestures that make that moment unique.
+ I know how valuable memories are and the power that photographs have to 
+evoke emotions and transport us in time. That's why I put all my heart and 
+talent into every image I take.
+ If you are looking for a photographer who captures real and authentic moments, 
+I invite you to explore my portfolio and discover my style.`,
+		'about.p6': `I am looking forward to being a part of your story and creating amazing photos 
+together.
+ Don't hesitate to contact me to discuss your ideas, answer your questions, or 
+book your photo session or event.`,
+		'about.p7': `I hope to meet you soon and have the opportunity to work with you.`,
+		'about.p8': `Are you looking for a photographer? I want to get to know you.`,
+		'about.contact': `I want you to be my photographer, Manu.`,
 		'photos.title': 'Photos',
 		'photos.business': 'Business',
 		'photos.families': 'Families',
@@ -74,11 +92,11 @@ export const ui = {
 		'photos.weddings': 'Weddings'
 	},
 	es: {
-		'nav.home': 'Inicio',
-		'nav.about': 'Sobre mí',
-		'nav.photos': 'Fotos',
-		'nav.weddings': 'Bodas',
-		'nav.contact': 'Contacto',
+		'nav.home': `Inicio`,
+		'nav.about': `Sobre mí`,
+		'nav.photos': `Fotos`,
+		'nav.weddings': `Bodas`,
+		'nav.contact': `Contacto`,
 		'header.title': `Hola! Soy Manuel, fotógrafo en Barcelona`,
 		'header.subtitle': 'Fotografía de bodas - eventos - familias - retratos - editoriales',
 		'photos.title': 'Fotos',
@@ -89,10 +107,10 @@ export const ui = {
 		'photos.weddings': 'Bodas'
 	},
 	fr: {
-		'nav.home': 'Accueil',
-		'nav.about': 'À propos',
-		'nav.photos': 'Photos',
-		'nav.weddings': 'Mariages',
+		'nav.home': `Accueil`,
+		'nav.about': `À propos`,
+		'nav.photos': `Photos`,
+		'nav.weddings': `Mariages`,
 		'header.title': `Bonjour! Je suis Manuel, photographe`,
 		'header.subtitle': 'Photographie de mariage - événements - familles - portraits - éditoriaux',
 		'photos.title': 'Photos',
