cf9c86360fa46e832d25dca565ef5beeaf4e22c7
Michaël Vanderheyden <hello@th3s4mur41.me>
2023-09-01 22:24:59 +0200

    feat: update color palette

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	plugins: [
 		require('postcss-nesting'),
 		require('autoprefixer'),
+		require('@csstools/postcss-oklab-function'),
 		require('postcss-preset-env')({
 			preserve: true
 		}),
